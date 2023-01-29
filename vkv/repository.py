bc76634277b05f66b10ebeb5628c7d418f5fcde0
Fernando Antunes <f.antunes84@gmail.com>
2023-01-29 00:59:41 -0300

    Refaturamento: criação das funções get_key_path e get_keys.

--- vkv/repository.py
+++ vkv/repository.py
@@ -15,24 +15,31 @@ def get_kv(program: str, key: str):
 
 
 def create_box():
-    with open('vkv.conf', 'r') as kd_file:
-        key_path = kd_file.readlines()[0].split('\n')[0]
-        kd_file.close()
+    key_path = get_key_path(0)
     with open(key_path, 'rb') as key_file:
         key = key_file.read()
         key_file.close()
     return nacl.secret.SecretBox(key)
 
 
-def set_env():
-    box = create_box()
+def get_key_path(path_type: int):
     with open('vkv.conf', 'r') as kd_file:
-        key_path = kd_file.readlines()[1].split('\n')[0]
+        key_path = kd_file.readlines()[path_type].split('\n')[0]
         kd_file.close()
+    return key_path
+
+
+def get_keys():
+    box = create_box()
+    key_path = get_key_path(1)
     with open(key_path, 'rb') as crypt_file:
         encrypted_data = crypt_file.read()
         crypt_file.close()
     data = box.decrypt(encrypted_data).decode()
-    keys = ast.literal_eval(data)
+    return ast.literal_eval(data)
+
+
+def set_env():
+    keys = get_keys()
     os.environ['URL'] = keys['url']
     os.environ['TOKEN'] = keys['token']
